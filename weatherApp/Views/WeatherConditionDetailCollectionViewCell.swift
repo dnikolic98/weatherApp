5ab0276945c3aef338327a55a2423f1e49c7ef68
Dario <dario.nikolic98@gmail.com>
2020-08-07 14:30:53 +0200

    updated design detailWeatherViewController

---
+++ weatherApp/Views/WeatherConditionDetailCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherConditionDetailCollectionViewCell.swift
+//  weatherApp
+//
+//  Created by Dario Nikolic on 07/08/2020.
+//  Copyright © 2020 Dario Nikolic. All rights reserved.
+//
+
+import UIKit
+
+class WeatherConditionDetailCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
