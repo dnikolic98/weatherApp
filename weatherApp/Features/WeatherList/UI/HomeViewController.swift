255d9ca25e6db2bb9bb1962d9644698804adbb0e
Dario <dario.nikolic98@gmail.com>
2020-09-17 13:37:52 +0200

    renamed viewcontroller level disposebag

--- weatherApp/Features/WeatherList/UI/HomeViewController.swift
+++ weatherApp/Features/WeatherList/UI/HomeViewController.swift
@@ -16,7 +16,7 @@ class HomeViewController: UIViewController {
     private var refreshControl: UIRefreshControl!
     private var currentWeatherListPresenter: CurrentWeatherListPresenter!
     private var dataDisposeBag: DisposeBag = DisposeBag()
-    private var timerDisposeBag: DisposeBag = DisposeBag()
+    private var viewControllerDisposeBag: DisposeBag = DisposeBag()
     private let dataRefreshPeriod: Int = 60 * 2
     
     @IBOutlet private weak var scrollView: UIScrollView!
@@ -90,14 +90,14 @@ class HomeViewController: UIViewController {
     }
     
     private func startTimer() {
-        timerDisposeBag = DisposeBag()
+        viewControllerDisposeBag = DisposeBag()
         
         Observable<Int>
             .timer(.seconds(0), period: .seconds(dataRefreshPeriod), scheduler: MainScheduler.instance)
             .subscribe(onNext: { [weak self] _ in
                 self?.bindViewModel()
             })
-            .disposed(by: timerDisposeBag)
+            .disposed(by: viewControllerDisposeBag)
     }
     
     //MARK: - UI elements setup
